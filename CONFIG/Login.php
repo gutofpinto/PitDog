1335f7bd8e7163378f570b3f710d25d7f554fd8a
Augusto_F_P <106616580+gutofpinto@users.noreply.github.com>
2022-08-22 22:29:03 -0300

    Update Login.php

--- CONFIG/Login.php
+++ CONFIG/Login.php
@@ -1,6 +1,5 @@
 <?php
     include("Conexao.php");
-
     $nome = filter_input(INPUT_POST, 'usuario_login');
     $senha = filter_input(INPUT_POST, 'senha_login');
     print("usuario ".$nome." e senha ".$senha);
